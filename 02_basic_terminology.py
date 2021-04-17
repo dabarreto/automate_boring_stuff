0202faa3512bd16c2fca9afb11a43b3abda7ba19
Daniela Barreto <anad.barreto@gmail.com>
2021-04-17 16:46:05 -0500

    adding basic terminology practice

---
+++ 02_basic_terminology.py
@@ -0,0 +1,71 @@
+# All expressions are just values and operators, and it always evaluates down to a single value.
+
+2 + 2
+
+(5 - 1) * ((7 + 1) / (3 - 1))
+
+
+# DATA TYPES. All values have a data type. A data type is a category of values, and every value belongs to exactly one data tyoe.
+
+# Integers "ints"
+
+-2
+
+30
+
+42
+
+# Floating point numbers "floats"
+
+3.14
+
+42.0
+
+# Text Values "strings"
+# When typing strings begin and end them with a single quote so Python knows where the text begins and ends.
+
+'Hello World'
+
+# String Concatenation
+# When the plus operator is used on strings it's the "string concatenartion" operator.
+# "joins the strings together"
+
+'Alice' + 'Bob'
+
+# String Replication
+# Operates on an integer and a string
+
+'Alice' * 3
+
+'Hello' + '!' * 10
+
+# Variables
+# Assignment Statement
+
+spam = 42
+
+spam
+
+spam = 'Hello'
+
+spam + ' World'
+
+
+# Statements
+# Unlike expressions, statements don't evaluate to a single value.
+
+spam = 2 + 2
+
+spam
+
+spam = 10
+
+spam = spam + 1
+
+
+
+
+
+
+
+
