f4d84e68db8fb3bcb68a29e840113a57c6467b40
Daniela Barreto <anad.barreto@gmail.com>
2021-04-17 16:49:00 -0500

    added if else elif practice

---
+++ 05_if_else_elif.py
@@ -0,0 +1,42 @@
+# The simplest flow control statements are If aand Else
+
+name = 'Alice'
+if name == 'Alice':
+    print('Hi Alice')
+print('Done')
+
+# The identation is how python tells what part is inside the IF statements block and what isn't. 
+# A block begins when the identation increases and ends when the identation returns to its previous level.
+
+# Blocks are also called clauses 
+# New blocks begin only after statements that end with a colon like in the IF statement
+
+# else statement
+# With the if else statements one of two blocks is executed
+
+password = 'swordfish'
+if password == 'swordfish':
+    print('Access granted.')
+else:
+    print('Wrong password.')
+
+# else if statement
+# One of many possible blocks to execute the if statement 
+
+name = 'Bob'
+age = 3000
+if name == 'Alice':
+    print('Hi Alice')
+elif age < 12
+    print('You are not Alice, kiddo.')
+elif age > 2000:
+    print('Unlike you, Alice is not an undead, immortal vampire.')
+elif age > 100:
+    print('You are not Alice, grannie.')
+
+
+
+
+
+
+
