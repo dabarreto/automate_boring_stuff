be597a4252b2fddb9c3b2b534982adbc5891d943
Daniela Barreto <anad.barreto@gmail.com>
2021-04-17 16:48:04 -0500

    added first python program practice

---
+++ 03_first_program.py
@@ -0,0 +1,48 @@
+# This program says hello ans asks for my name
+
+print('Hello World')
+
+print('What is your name?') # ask for their name
+myName = input()
+print('It is goof to meet you, ' + myName)
+print('The length of your name is:')
+print(len(myName))
+
+print('What is your age?') #ask for their age
+myAge = input()
+print('You will be ' + str(int(myAge) + 1) + ' in a year.')
+
+
+# Values passed to functions = Arguments
+
+# print function : print() displays the value passed to it.
+
+# input function: input() lets user type in a value. python waits for the user to type some text on a keryboard and press enter.
+
+# len function: len() takes a string value and returns an integer value of the string's length. returns the number characters in a string.
+
+# int(), str(), and floar() convert values' data types.
+
+len ('Al')
+
+len('')
+
+len ('Al') * 10
+
+str(25)
+
+int('1234')
+
+float('3.14')
+
+float(1)
+
+int('26') + 1
+
+27 + 'years old.'
+
+# Recap:
+# Type programs into the file editor
+# The execution starts at the top and moves down
+# '#' Comments are ignored by python
+# Functions are mini-programs in your program
