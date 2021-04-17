408946b4af7eb09f83fccf761e94e7cc72178c6e
Daniela Barreto <anad.barreto@gmail.com>
2021-04-17 16:48:34 -0500

    added basic flow control concepts

---
+++ 04_basic_flow_control_concepts.py
@@ -0,0 +1,57 @@
+# Boolean Values
+# Comparison Operators
+# Boolean Operators
+
+# Boolean data type has only two values: True and False.
+# Like any other value, boolean values are used in expression and can be stored in variables.
+
+spam = True
+
+# Comparison Operators
+# ----------------------------------
+#   ==  Equal to
+#   !=  Not equal to
+#   <   Less than
+#   <   Greater than
+#   <=  Less than or equal to
+#   >=  Greater than or equal to
+# ----------------------------------
+
+42 == 42
+
+42 == 'Hello'
+
+2 != 3
+
+42 < 100
+
+42 >= 100
+
+42 <= 42
+
+myAge = 26
+
+myAge < 30
+
+42 == '42'
+
+42.0 == 42
+
+# Boolean Operators: and, or, not.
+
+# The "and" operator evaluates an expression to rue if both boolean values are True. Otherwise it evaluates to False.
+ 
+ True and True
+
+ True and False
+
+ False and True
+
+# The "or" operator
+# The "not" operator
+
+myAge = 26
+myPet = 'cat'
+myAge > 20 and myPet == 'cat'
+
+
